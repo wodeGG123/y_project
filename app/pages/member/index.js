20799d62846a9ddd3d1d645ed37a4c79d0033044
panda.cn <65720230@qq.com>
2018-01-29 23:49:15 +0800

    18-1-29-2

--- app/pages/member/index.js
+++ app/pages/member/index.js
@@ -63,11 +63,11 @@ export default class Main extends Component {
               </View>
               <View style={styles.memberAlertTextBlock}>
                 <View style={styles.memberAlertTextWrap}>
-                  <Text style={styles.memberAlertText}>忘记密码？</Text>  
+                  <Text onPress={()=>{this.props.navigation.navigate('Forget')}} style={styles.memberAlertText}>忘记密码？</Text>
                 </View>
                 <View style={styles.memberAlertTextWrap}>
-                  <Text style={styles.memberAlertText}>没有账号，立即</Text>
-                  <Text onPress={()=>{this.props.navigation.navigate('Regist')}} style={{color:'#3385ff',fontSize: 12}}>注册</Text>
+                  <Text style={styles.memberAlertText}>没有账号，</Text>
+                  <Text onPress={()=>{this.props.navigation.navigate('Regist')}} style={{color:'#3385ff',fontSize: 12}}>立即注册</Text>
                 </View>
               </View>
           </View>
@@ -184,10 +184,68 @@ class Forget extends Component {
 
   render() {
     
-    return (
-      <View style={styles.container}>
-          <Text>Forget</Text>
-      </View>
+    return (<View style={styles.container}>
+            <View style={styles.memberBlock}>
+                <View style={{height:44}}></View>
+                <View style={styles.memberInputBlock}>
+                    <View style={styles.memberIconWrap} >
+                        <Icon  style={styles.memberIcon} size={24} name='md-person' />
+                    </View>
+                    <View style={styles.memberInputWrap}>
+                        <TextInput
+                            editable={true}
+                            style={styles.memberInput}
+                            maxLength={11}
+                            placeholder='手机号'
+                        />
+                    </View>
+
+                </View>
+                <View style={{width:deviceWidth-40,borderBottomColor: '#eee',borderBottomWidth: 1}}></View>
+                <View style={styles.memberInputBlock}>
+                    <View style={styles.memberIconWrap}>
+                        <Icon style={styles.memberIcon} size={24} name='md-code' />
+                    </View>
+                    <View style={styles.memberInputWrap}>
+                        <TextInput
+                            editable={true}
+                            style={styles.memberInput}
+                            maxLength={16}
+                            placeholder='验证码'
+                        />
+                    </View>
+                    <Button style={styles.memberSendCode} textStyle={styles.memberSendCodeText}>
+                        发送验证码
+                    </Button>
+                </View>
+                <View style={{width:deviceWidth-40,borderBottomColor: '#eee',borderBottomWidth: 1}}></View>
+                <View style={styles.memberInputBlock}>
+                    <View style={styles.memberIconWrap}>
+                        <Icon style={styles.memberIcon} size={24} name='md-unlock' />
+                    </View>
+                    <View style={styles.memberInputWrap}>
+                        <TextInput
+                            editable={true}
+                            style={styles.memberInput}
+                            maxLength={16}
+                            placeholder='填写新密码'
+                        />
+                    </View>
+
+                </View>
+                <View style={styles.memberButtonWrap}>
+                    <Button style={styles.memberButton} textStyle={styles.memberButtonText}>
+                        提交
+                    </Button>
+                </View>
+                <View style={styles.memberAlertTextBlock}>
+                    <View style={styles.memberAlertTextWrap}>
+                        <Text style={styles.memberAlertText}>已有账号，立即</Text>
+                        <Text onPress={()=>{this.props.navigation.navigate('Login')}} style={{color:'#3385ff',fontSize: 12}}>登录</Text>
+                    </View>
+                </View>
+            </View>
+        </View>
     );
   }
 }
